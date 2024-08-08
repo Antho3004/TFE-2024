eafb1b3d1d9696ef938076a17af77c42935f8623
Antho3004 <anthony.iv14@gmail.com>
2024-08-08 17:50:24 +0200

    L'utilisateur peut désormais ajouter un type de réveil sur le calendrier et également la modifier en cliquant sur l'heure type dans le calendrier

--- connexion.py
+++ connexion.py
@@ -1,19 +1,36 @@
 import sqlite3
 from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
 
 bcrypt = Bcrypt()
+
+class User(UserMixin):
+    def __init__(self, id, nom, email):
+        self.id = id
+        self.nom = nom
+        self.email = email
+
+    @staticmethod
+    def get(user_id):
+        try:
+            with sqlite3.connect('data.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT id_utilisateur, nom, email FROM utilisateurs WHERE id_utilisateur = ?', (user_id,))
+                user = cursor.fetchone()
+                if user:
+                    return User(user[0], user[1], user[2])
+        except sqlite3.Error as e:
+            print(f"Erreur SQLite: {e}")
+        return None
 
 def recherche_utilisateur(nom_utilisateur, mot_de_passe):
     try:
         with sqlite3.connect('data.db') as conn:
             cursor = conn.cursor()
-            cursor.execute('''
-                SELECT id_utilisateur, nom, mot_de_passe FROM utilisateurs WHERE nom = ?
-            ''', (nom_utilisateur,))
+            cursor.execute('SELECT id_utilisateur, nom, email, mot_de_passe FROM utilisateurs WHERE nom = ?', (nom_utilisateur,))
             utilisateur = cursor.fetchone()
-            if utilisateur and utilisateur[2]:
-                if bcrypt.check_password_hash(utilisateur[2], mot_de_passe):
-                    return {"id": utilisateur[0], "nom": utilisateur[1]}
+            if utilisateur and utilisateur[3] and bcrypt.check_password_hash(utilisateur[3], mot_de_passe):
+                return User(utilisateur[0], utilisateur[1], utilisateur[2])
     except sqlite3.Error as e:
         print(f"Erreur SQLite: {e}")
     return None
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from inscription import ajouter_utilisateur, bcrypt
-from connexion import recherche_utilisateur
+from connexion import recherche_utilisateur, User
 from heure_type import ajouter_heure_type, obtenir_heures_type, obtenir_heure_type, modifier_heure_type, supprimer_heure_type
 
 app = Flask(__name__)
 app.secret_key = "2fb48ed61fe5f62c7ea52bd58128ca1179fdd8b162ac4cacc3da03e83a0b053f"
 
-@app.route("/")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/')
 def index():
     return redirect(url_for('login'))
 
@@ -20,8 +29,7 @@ def inscription():
 
         if ajouter_utilisateur(nom, prenom, email, mot_de_passe):
             utilisateur = recherche_utilisateur(nom, mot_de_passe)
-            session['nom_utilisateur'] = utilisateur['nom']
-            session['id_utilisateur'] = utilisateur['id_utilisateur']
+            login_user(utilisateur)
             flash('Inscription réussie et connecté!', 'success')
             return redirect(url_for('traitement'))
         else:
@@ -30,44 +38,36 @@ def inscription():
 
     return render_template('inscription.html')
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        donnees = request.form
-        nom = donnees.get('nom')
-        mdp = donnees.get('mot_de_passe')
-
-        utilisateur = recherche_utilisateur(nom, mdp)
-
-        if utilisateur is not None:
-            session['nom_utilisateur'] = utilisateur['nom']
-            session['id_utilisateur'] = utilisateur['id']
+    if request.method == 'POST':
+        nom = request.form['nom']
+        mot_de_passe = request.form['mot_de_passe']
+        utilisateur = recherche_utilisateur(nom, mot_de_passe)
+        
+        if utilisateur:
+            login_user(utilisateur)
             return redirect(url_for('traitement'))
         else:
             flash('Nom d\'utilisateur ou mot de passe incorrect', 'danger')
-            return redirect(request.url)
-    else:
-        if 'nom_utilisateur' in session:
-            return redirect(url_for('traitement'))
-        return render_template("login.html")
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('nom_utilisateur', None)
-    session.pop('id_utilisateur', None)
+    logout_user()
     return redirect(url_for('login'))
 
-@app.route("/traitement", methods=["POST", "GET"])
+@app.route('/traitement', methods=['GET', 'POST'])
+@login_required
 def traitement():
-    if 'nom_utilisateur' not in session:
-        return redirect(url_for('login'))
-
-    if request.method == "POST":
+    if request.method == 'POST':
         heure = int(request.form['heure'])
         minutes = int(request.form['minutes'])
         heure_combinee = heure * 100 + minutes
         date = request.form['date']
-        id_utilisateur = session['id_utilisateur']
+        id_utilisateur = current_user.id
 
         if ajouter_heure_type(heure_combinee, date, id_utilisateur):
             flash('Heure de réveil ajoutée avec succès!', 'success')
@@ -76,18 +76,16 @@ def traitement():
         
         return redirect(url_for('traitement'))
     else:
-        id_utilisateur = session['id_utilisateur']
+        id_utilisateur = current_user.id
         heures = obtenir_heures_type(id_utilisateur)
         return render_template("traitement.html", heures=heures)
 
-@app.route("/modifier/<int:heure>/<date>", methods=["GET", "POST"])
+@app.route('/modifier/<int:heure>/<date>', methods=['GET', 'POST'])
+@login_required
 def modifier_heure_route(heure, date):
-    if 'nom_utilisateur' not in session:
-        return redirect(url_for('login'))
+    id_utilisateur = current_user.id
 
-    id_utilisateur = session['id_utilisateur']
-
-    if request.method == "POST":
+    if request.method == 'POST':
         nouvelle_heure = int(request.form['heure'])
         nouvelle_minutes = int(request.form['minutes'])
         nouvelle_heure_combinee = nouvelle_heure * 100 + nouvelle_minutes
@@ -107,12 +105,10 @@ def modifier_heure_route(heure, date):
             flash('Heure de réveil non trouvée.', 'danger')
             return redirect(url_for('traitement'))
 
-@app.route("/supprimer/<int:heure>/<date>")
+@app.route('/supprimer/<int:heure>/<date>')
+@login_required
 def supprimer_heure_route(heure, date):
-    if 'nom_utilisateur' not in session:
-        return redirect(url_for('login'))
-
-    id_utilisateur = session['id_utilisateur']
+    id_utilisateur = current_user.id
 
     if supprimer_heure_type(heure, date, id_utilisateur):
         flash('Heure de réveil supprimée avec succès!', 'success')
@@ -122,11 +118,9 @@ def supprimer_heure_route(heure, date):
     return redirect(url_for('traitement'))
 
 @app.route('/get_heures')
+@login_required
 def get_heures():
-    if 'id_utilisateur' not in session:
-        return redirect(url_for('login'))
-
-    id_utilisateur = session['id_utilisateur']
+    id_utilisateur = current_user.id
     heures = obtenir_heures_type(id_utilisateur)
 
     events = []
@@ -140,38 +134,33 @@ def get_heures():
     return jsonify(events)
 
 @app.route('/add_heure', methods=['POST'])
+@login_required
 def add_heure():
-    if 'id_utilisateur' not in session:
-        return redirect(url_for('login'))
-
-    heure = int(request.form['heure'])
-    date = request.form['date']
-    id_utilisateur = session['id_utilisateur']
+    data = request.get_json()
+    heure = int(data['heure'])
+    date = data['date']
+    id_utilisateur = current_user.id
 
     ajouter_heure_type(heure, date, id_utilisateur)
     return '', 204
 
 @app.route('/delete_heure', methods=['POST'])
+@login_required
 def delete_heure():
-    if 'id_utilisateur' not in session:
-        return redirect(url_for('login'))
-
     heure = int(request.form['heure'])
     date = request.form['date']
-    id_utilisateur = session['id_utilisateur']
+    id_utilisateur = current_user.id
 
     supprimer_heure_type(heure, date, id_utilisateur)
     return '', 204
 
 @app.route('/modify_heure', methods=['POST'])
+@login_required
 def modify_heure():
-    if 'id_utilisateur' not in session:
-        return redirect(url_for('login'))
-
     old_heure = int(request.form['old_heure'])
     new_heure = int(request.form['new_heure'])
     date = request.form['date']
-    id_utilisateur = session['id_utilisateur']
+    id_utilisateur = current_user.id
 
     if modifier_heure_type(old_heure, date, new_heure, date, id_utilisateur):
         return '', 204
